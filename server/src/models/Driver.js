1544b77413089ef3eba592ad34c9d5b1da2e4040
puchiflito <aguskainer1998@outlook.com>
2024-05-09 09:59:16 -0300

    Modelos y relaciones creadas

--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -1,10 +1,38 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
+
+const { v4: uuid } = require("uuid");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Driver', {
+  sequelize.define("Driver", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: () => uuid(),
+      primaryKey: true,
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      defaultValue:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Lance_Reventlow_and_actress_Cheryl_Holdridge_wedding_portrait%2C_Calif.%2C_1964.jpg/800px-Lance_Reventlow_and_actress_Cheryl_Holdridge_wedding_portrait%2C_Calif.%2C_1964.jpg",
+    },
+    nationality: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthdate: {
       type: DataTypes.STRING,
       allowNull: false,
     },
