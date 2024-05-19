40817ca4e7fb5ecc5791e3dc4d8e58039f7713b0
puchiflito <aguskainer1998@outlook.com>
2024-05-19 09:44:01 -0300

    agregar boton de ir al home

--- client/src/Views/landingPage/LandingPage.jsx
+++ client/src/Views/landingPage/LandingPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import style from "./landingPage.module.css";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <div className={style.body}>
       <h1>Bienvenidos a la App de la Formula 1</h1>
+      <Link to="/home">
+        <button>Entrar</button>
+      </Link>
     </div>
   );
 };
